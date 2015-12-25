fc38f2fbb3716ff66dbff0cc038d43ecd1ec560c
Artem Yarulin <artem.yarulin@fessguid.com>
2015-12-25 08:28:07 +0200

    Prepeare for the first release
    react-native-externs added to the deps

--- project.clj
+++ project.clj
@@ -1,8 +1,9 @@
-(defproject ktoa "0.0.1"
+(defproject ktoa "0.0.1-SNAPSHOT"
   :description "Set of useful helpers and wrappers around React Native for ClojureScript development"
   :url "https://github.com/artemyarulin/ktoa"
   :dependencies [[org.clojure/clojure "1.7.0"]
-                 [org.clojure/clojurescript "1.7.122"]]
+                 [org.clojure/clojurescript "1.7.122"]
+                 [react-native-externs "0.0.1-SNAPSHOT"]
   :plugins [[lein-cljsbuild "1.1.0"]]
   :source-paths ["src"]
   :cljsbuild {:builds {:repl {:source-paths ["src"]}}})
