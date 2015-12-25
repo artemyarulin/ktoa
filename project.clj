b0eec648504243d83d9adc7ec66f5d64abce80ca
Artem Yarulin <artem.yarulin@fessguid.com>
2015-12-25 13:44:27 +0200

    License added

--- project.clj
+++ project.clj
@@ -1,6 +1,8 @@
 (defproject ktoa "0.0.1-SNAPSHOT"
   :description "Set of useful helpers and wrappers around React Native for ClojureScript development"
   :url "https://github.com/artemyarulin/ktoa"
+  :license {:name "MIT License"
+            :url "http://opensource.org/licenses/MIT"}
   :dependencies [[org.clojure/clojure "1.7.0"]
                  [org.clojure/clojurescript "1.7.122"]]
   :plugins [[lein-cljsbuild "1.1.0"]]
