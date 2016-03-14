eea4f276695771c6647600d699590b9796703112
Artem Yarulin <artem.yarulin@fessguid.com>
2016-03-14 17:33:11 +0200

    Bump release

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject ktoa "0.1.2-SNAPSHOT"
+(defproject ktoa "0.1.3-SNAPSHOT"
   :description "Set of useful helpers and wrappers around React Native for ClojureScript development"
   :url "https://github.com/artemyarulin/ktoa"
   :license {:name "MIT License"
